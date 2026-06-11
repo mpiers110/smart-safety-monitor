ddf913684f613a25eab39dc7e497e07d41ce1ef6
Tech Tafari <omitihenry@gmail.com>
2026-06-11 08:58:07 +0300

    Add GATT profile header for BLE service and characteristics

---
+++ esp32-ble/gatt_profile.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace ssm {
+
+// Service
+#define SSM_SERVICE_UUID              "a12f0001-4b8f-4c3d-9a12-7f1a2b3c4d5e"
+
+// Characteristics
+#define SSM_CHAR_LIVE_DATA_UUID       "a12f0002-4b8f-4c3d-9a12-7f1a2b3c4d5e"
+#define SSM_CHAR_DIAGNOSTICS_UUID     "a12f0003-4b8f-4c3d-9a12-7f1a2b3c4d5e"
+#define SSM_CHAR_COMMANDS_UUID        "a12f0004-4b8f-4c3d-9a12-7f1a2b3c4d5e"
+#define SSM_CHAR_STATUS_UUID         "a12f0005-4b8f-4c3d-9a12-7f1a2b3c4d5e"
+
+}
